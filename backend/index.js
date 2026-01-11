0540dd079e7cf32fd123efe5db3fe598b57b3ce3
sanj4git <149954296+sanj4git@users.noreply.github.com>
2026-01-11 12:24:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
-const messageRoutes = require("./routes/message.routes");
 
-
-
-require("dotenv").config({
-    path: path.resolve(__dirname, "../.env"),
-  });
-
-
-app.use(cors());
 const authRoutes = require("./routes/auth.routes");
 const projectRoutes = require("./routes/project.routes");
 const uploadRoutes = require("./routes/upload.routes");
 const queryRoutes = require("./routes/query.routes");
-
-
-
+const messageRoutes = require("./routes/message.routes");
 
 // Middleware
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://consensus-xisa.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
@@ -35,8 +33,10 @@ app.use("/messages", messageRoutes);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(err => console.log(err));
